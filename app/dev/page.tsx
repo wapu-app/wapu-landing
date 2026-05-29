5f137fcc500cefe494d778ec42e09c54501b891a
ezequiel <alcayaga147147@gmail.com>
2026-05-29 17:26:11 -0300

    wapu dev responsive

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -145,7 +145,7 @@ export default function DevPage() {
             <a href="#flujo">Flujo</a>
             <a href="#casos">Casos de uso</a>
             <span className="dev-nav-sep" aria-hidden="true" />
-            <a className="dev-nav-ghost" href="/api-docs">API Docs</a>
+            <a className="dev-nav-ghost dev-nav-hide-sm" href="/api-docs">API Docs</a>
             <a className="dev-nav-ghost" href={GITHUB_CLI} target="_blank" rel="noreferrer">
               <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M12 .5C5.7.5.5 5.7.5 12c0 5.1 3.3 9.4 7.9 10.9.6.1.8-.2.8-.5v-2c-3.2.7-3.9-1.4-3.9-1.4-.5-1.3-1.3-1.7-1.3-1.7-1.1-.7.1-.7.1-.7 1.2.1 1.8 1.2 1.8 1.2 1 1.8 2.7 1.3 3.4 1 .1-.8.4-1.3.8-1.6-2.6-.3-5.3-1.3-5.3-5.8 0-1.3.5-2.3 1.2-3.1-.1-.3-.5-1.5.1-3.1 0 0 1-.3 3.3 1.2a11.4 11.4 0 0 1 6 0C17.3 4.7 18.3 5 18.3 5c.6 1.6.2 2.8.1 3.1.8.8 1.2 1.8 1.2 3.1 0 4.5-2.7 5.5-5.3 5.8.4.4.8 1.1.8 2.2v3.3c0 .3.2.6.8.5 4.6-1.5 7.9-5.8 7.9-10.9C23.5 5.7 18.3.5 12 .5Z" /></svg>
               GitHub
